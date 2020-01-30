eedc1b1c833a244ccccf3d4c8f16dd3e92b7beca
Maarten <mdepypere@gmail.com>
2020-01-30 23:48:46 +0000

    Update vars.php

--- vars.php
+++ vars.php
@@ -4,4 +4,15 @@ $pdohost = '';
 $pdouser = '';
 $pdopass = '';
 $pdodbname = '';
+// variables for emails
+$mailhost = 'smtp.gmail.com';  						  // Specify main and backup SMTP servers
+$mailport = 465; 									  // TCP port to connect to
+$mailsmtpauth = true;                               // Enable SMTP authentication
+$mailusername = '';              
+$mailpassword = '';                      
+$mailsmtpecure = 'ssl';
+$mailfromaddress = '';
+$mailfromname = '';
+// for date conversion
+$GLOBALS['mysqldateformat'] = '%d-%m-%Y';
 ?>
