4dabc5366f7244963b8bb7a9bd510f1dd8329991
Maarten <mdepypere@gmail.com>
2021-06-09 22:26:16 +0200

    extend php variables with defaults for date conversion

--- vars.php
+++ vars.php
@@ -5,14 +5,23 @@ $pdouser = '';
 $pdopass = '';
 $pdodbname = '';
 // variables for emails
-$mailhost = 'smtp.gmail.com';  						  // Specify main and backup SMTP servers
+$mailhost = '';  						  // Specify main and backup SMTP servers
 $mailport = 465; 									  // TCP port to connect to
 $mailsmtpauth = true;                               // Enable SMTP authentication
 $mailusername = '';                // SMTP username
 $mailpassword = '';                        // SMTP password
 $mailsmtpecure = 'ssl';
 $mailfromaddress = '';
-$mailfromname = '';
+$mailvars = [
+  "mailhost" => $mailhost,
+  "mailport" => $mailport,
+  "mailsmtpauth" => $mailsmtpauth,
+  "mailusername" => $mailusername,
+  "mailpassword" => $mailpassword,
+  "mailsmtpecure" => $mailsmtpecure,
+  "mailfromaddress" => $mailfromaddress,
+];
 // for date conversion
 $GLOBALS['mysqldateformat'] = '%d-%m-%Y';
+$GLOBALS['phpdateformat'] = '%Y-%m-%d';
 ?>
